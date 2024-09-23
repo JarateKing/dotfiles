717d8717edba4e30e2a1511653653ab2d9bc7e3e
JarateKing <scmacintosh@upei.ca>
2024-09-23 09:28:01 -0300

    simplify window

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,3 +2,9 @@
 (setq inhibit-startup-screen t)
 (setq initial-scratch-message "")
 
+;; style
+(set-fringe-mode 0)    ; remove padding on sides
+(scroll-bar-mode -1)   ; remove scrollbars
+(menu-bar-mode -1)     ; remove dropdown menus
+(tool-bar-mode -1)     ; remove toolbar ribbon
+
