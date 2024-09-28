08d62b9510b1a519491cd1e4e6151775233bbf31
JarateKing <scmacintosh@upei.ca>
2024-09-27 22:30:16 -0300

    added ctrl+s and ctrl+f binds

--- .emacs.d/keybinds.el
+++ .emacs.d/keybinds.el
@@ -1,6 +1,9 @@
 (keymap-global-set "C-v" 'yank)
+(keymap-global-set "C-s" 'save-buffer)
 
 (keymap-global-set "C-i" 'previous-line)
 (keymap-global-set "C-k" 'next-line)
 (keymap-global-set "C-j" 'left-char)
 (keymap-global-set "C-l" 'right-char)
+
+(keymap-global-set "C-f" 'isearch-forward)
