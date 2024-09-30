d396068ada08c04a6f702b38265e8e6b891fe688
JarateKing <scmacintosh@upei.ca>
2024-09-29 21:18:47 -0300

    comment sections

--- .emacs.d/keybinds.el
+++ .emacs.d/keybinds.el
@@ -1,9 +1,10 @@
+;; some common keybinds
 (keymap-global-set "C-v" 'yank)
 (keymap-global-set "C-s" 'save-buffer)
+(keymap-global-set "C-f" 'isearch-forward)
 
+;; IJKL navigation
 (keymap-global-set "C-i" 'previous-line)
 (keymap-global-set "C-k" 'next-line)
 (keymap-global-set "C-j" 'left-char)
 (keymap-global-set "C-l" 'right-char)
-
-(keymap-global-set "C-f" 'isearch-forward)
