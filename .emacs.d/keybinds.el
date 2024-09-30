9ce74332db7eaa5c7c7040afc1e9f36c505f0b76
JarateKing <scmacintosh@upei.ca>
2024-09-30 17:55:40 -0300

    fixed buggy keybinds

--- .emacs.d/keybinds.el
+++ .emacs.d/keybinds.el
@@ -1,10 +1,6 @@
-;; some common keybinds
-(keymap-global-set "C-v" 'yank)
-(keymap-global-set "C-s" 'save-buffer)
-(keymap-global-set "C-f" 'isearch-forward)
+(require 'bind-key)
 
-;; IJKL navigation
-(keymap-global-set "C-i" 'previous-line)
-(keymap-global-set "C-k" 'next-line)
-(keymap-global-set "C-j" 'left-char)
-(keymap-global-set "C-l" 'right-char)
+;; some common keybinds
+(bind-key* "C-v" 'yank)
+(bind-key* "C-s" 'save-buffer)
+(bind-key* "C-f" 'isearch-forward)
