83427b87b88df823f2e97188bfc248f548b9edcf
JarateKing <scmacintosh@upei.ca>
2024-10-18 21:53:45 -0300

    remove mouse accel

--- scripts/on_dotfiles_install.sh
+++ scripts/on_dotfiles_install.sh
@@ -1,2 +1,7 @@
-## remap capslock to ctrl
+## Gnome configuration
+
+### remap capslock to ctrl
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+
+### no mouse accel
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile "flat"
