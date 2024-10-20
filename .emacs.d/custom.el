c646eafce6876bd2b7adbef00569cd1e8e95f2c5
JarateKing <scmacintosh@upei.ca>
2024-10-20 12:15:01 -0300

    updated custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -9,4 +9,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(whitespace-empty ((t (:foreground "#1e2a38"))))
+ '(whitespace-missing-newline-at-eof ((t (:foreground "#1e2a38"))))
+ '(whitespace-newline ((t (:foreground "#1e2a38"))))
+ '(whitespace-space ((t (:foreground "#1e2a38"))))
+ '(whitespace-space-after-tab ((t (:foreground "#1e2a38"))))
+ '(whitespace-space-before-tab ((t (:foreground "#1e2a38"))))
+ '(whitespace-tab ((t (:foreground "#1e2a38"))))
+ '(whitespace-trailing ((t (:foreground "#1e2a38")))))
