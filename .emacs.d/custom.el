b7941cd80711bc0903eb35c58c594231815d7bea
JarateKing <scmacintosh@upei.ca>
2024-10-20 12:52:56 -0300

    updated custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -9,6 +9,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "#d26246"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#af7a00"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "#7c8d00"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "#00994e"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "#009b82"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "#0092c4"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "#737eca"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "#a66fb6"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "#d15f6d"))))
  '(whitespace-empty ((t (:foreground "#1e2a38"))))
  '(whitespace-missing-newline-at-eof ((t (:foreground "#1e2a38"))))
  '(whitespace-newline ((t (:foreground "#1e2a38"))))
