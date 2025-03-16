bf6ba7246c9d3090e3e648ca7e017981c09eca4c
JarateKing <scmacintosh@upei.ca>
2025-03-15 21:28:58 -0300

    added some window settings

--- .emacs.d/post-init.el
+++ .emacs.d/post-init.el
@@ -1,5 +1,11 @@
 ;;; post-init.el --- Custom Init -*- no-byte-compile: t; lexical-binding: t; -*-
 
-;; themes
+;; theme
 (mapc #'disable-theme custom-enabled-themes)
 (load-theme 'custom t)
+(add-to-list 'default-frame-alist '(font . "JetBrains Mono-10"))
+
+;; window
+(if (window-system) (set-frame-size (selected-frame) 180 40)) ; default window size
+(setq-default frame-title-format '("%b %f"))                  ; simplify window title
+(set-fringe-mode 0)                                           ; remove padding
