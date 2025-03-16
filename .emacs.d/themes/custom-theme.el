5a2d3a2bde465c97ac4a89f566ffb9bcc07821fa
JarateKing <scmacintosh@upei.ca>
2025-03-15 22:25:31 -0300

    hide vertical border

--- .emacs.d/themes/custom-theme.el
+++ .emacs.d/themes/custom-theme.el
@@ -9,6 +9,7 @@
    `(default ((,class (:background "#0b1118" :foreground "#d3e8f3" :font "JetBrains Mono-10"))))
    `(cursor ((,class (:background "#55BCDA"))))
    `(fringe ((,class ())))
+   `(vertical-border ((,class (:foreground "#0b1118"))))
    `(region ((,class (:background "#123040"))))
    `(mode-line ((,class (:background "#0B4677" :foreground "#FFFFFF"))))
    `(mode-line-inactive ((,class (:background "#0B2A4E" :foreground "#AAAAAA"))))))
