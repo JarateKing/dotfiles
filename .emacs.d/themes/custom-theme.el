7abbc9a6efe5269653b511bfb2c3aa10bd07a37f
JarateKing <scmacintosh@upei.ca>
2025-03-20 22:36:25 -0300

    made theme whitespace/formatting consistent

--- .emacs.d/themes/custom-theme.el
+++ .emacs.d/themes/custom-theme.el
@@ -7,11 +7,11 @@
   (custom-theme-set-faces
    'custom
    `(default ((,class (:background "#0b1118" :foreground "#d3e8f3" :font "JetBrains Mono-10"))))
-   `(cursor ((,class (:background "#55BCDA"))))
+   `(cursor ((,class (:background "#55bcda"))))
    `(fringe ((,class ())))
    `(vertical-border ((,class (:foreground "#0b1118"))))
    `(region ((,class (:background "#123040"))))
-   `(mode-line ((,class (:background "#0B4677" :foreground "#ffffff" :box nil))))
+   `(mode-line ((,class (:background "#0b4677" :foreground "#ffffff" :box nil))))
    `(mode-line-inactive ((,class (:background "#0b2a4e" :foreground "#aaaaaa" :box nil))))
    `(font-lock-builtin-face ((,class (:foreground "#00c27a"))))
    `(font-lock-constant-face ((,class (:foreground "#54a3ff"))))
@@ -22,14 +22,14 @@
    `(font-lock-type-face ((,class (:foreground "#f874bf"))))
    `(font-lock-variable-name-face ((,class (:foreground "#fe8581"))))
    `(font-lock-warning-face ((,class (:foreground "#ff002d" :weight bold))))
-   `(whitespace-newline                ((t (:foreground, "#1e2a38"))))
+   `(whitespace-newline ((t (:foreground, "#1e2a38"))))
    `(whitespace-missing-newline-at-eof ((t (:foreground, "#1e2a38"))))
-   `(whitespace-space                  ((t (:foreground, "#1e2a38"))))
-   `(whitespace-space-after-tab        ((t (:foreground, "#1e2a38"))))
-   `(whitespace-space-before-tab       ((t (:foreground, "#1e2a38"))))
-   `(whitespace-tab                    ((t (:foreground, "#1e2a38"))))
-   `(whitespace-trailing               ((t (:foreground, "#1e2a38"))))
-   `(whitespace-empty                  ((t (:foreground, "#1e2a38"))))
+   `(whitespace-space ((t (:foreground, "#1e2a38"))))
+   `(whitespace-space-after-tab ((t (:foreground, "#1e2a38"))))
+   `(whitespace-space-before-tab ((t (:foreground, "#1e2a38"))))
+   `(whitespace-tab ((t (:foreground, "#1e2a38"))))
+   `(whitespace-trailing ((t (:foreground, "#1e2a38"))))
+   `(whitespace-empty ((t (:foreground, "#1e2a38"))))
    `(powerline-active0 ((,class (:background "#0b4677" :foreground "#ffffff"))))
    `(powerline-active1 ((,class (:background "#113354" :foreground "#ffffff"))))
    `(powerline-active2 ((,class (:background "#112234" :foreground "#ffffff"))))
@@ -54,5 +54,3 @@
    `(neo-file-link-face ((,class (:foreground "#d3e8f3"))))))
 
 (provide-theme 'custom)
-
-
