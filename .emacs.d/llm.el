0feab505970ddd1497a73f2a0e9d63fb09e254ce
JarateKing <scmacintosh@upei.ca>
2025-05-20 18:13:56 -0300

    added translate to english llm prompt keybind

--- .emacs.d/llm.el
+++ .emacs.d/llm.el
@@ -25,8 +25,23 @@
       :system "Please give a short description of this word, phrase, or code snippet. Try to give some example usages and related words/phrases/functions/etc."
       :context input)))
 
+(defun llm-translate-to-english (start end)
+  "Use an LLM to translate the current region to English."
+  (interactive "r")
+  (unless (region-active-p)
+    (error "you must have a region set"))
+  (message "running LLM prompt...")
+  (let ((input (buffer-substring-no-properties (region-beginning) (region-end))))
+    (gptel-request nil
+      :callback (lambda (response info)
+        (with-output-to-temp-buffer "*LLM*"
+          (print response)))
+      :system "Please translate the following word or phrase into English. Then explain some of the word choice and potential other translations, and explain some cultural nuances that might be unclear from just the direct translation."
+      :context input)))
+
 (bind-key* "C-o c" 'gptel) ; chat window
 (bind-key* "C-o i" 'gptel-send) ; insert from prompt
 (bind-key* "C-o r" 'gptel-rewrite) ; rewrite selection with prompt
 (bind-key* "C-o q" 'gptel-abort) ; quit llm usage
 (bind-key* "C-o e" 'llm-explain-region) ; explain region
+(bind-key* "C-o t" 'llm-translate-to-english) ; translate region to english
