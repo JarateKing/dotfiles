a4aaa47f5b65e7b63d8ef93f5a8b36796dc1a773
JarateKing <scmacintosh@upei.ca>
2025-05-20 18:01:04 -0300

    added keybind to explain region using llm

--- .emacs.d/llm.el
+++ .emacs.d/llm.el
@@ -11,7 +11,22 @@
                  :stream t
                  :models '(mistral))))
 
+(defun llm-explain-region (start end)
+  "Use an LLM to describe the current region."
+  (interactive "r")
+  (unless (region-active-p)
+    (error "you must have a region set"))
+  (message "running LLM prompt...")
+  (let ((input (buffer-substring-no-properties (region-beginning) (region-end))))
+    (gptel-request nil
+      :callback (lambda (response info)
+        (with-output-to-temp-buffer "*LLM*"
+          (print response)))
+      :system "Please give a short description of this word, phrase, or code snippet. Try to give some example usages and related words/phrases/functions/etc."
+      :context input)))
+
 (bind-key* "C-o c" 'gptel) ; chat window
 (bind-key* "C-o i" 'gptel-send) ; insert from prompt
 (bind-key* "C-o r" 'gptel-rewrite) ; rewrite selection with prompt
 (bind-key* "C-o q" 'gptel-abort) ; quit llm usage
+(bind-key* "C-o e" 'llm-explain-region) ; explain region
