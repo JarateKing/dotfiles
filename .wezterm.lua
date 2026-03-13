7b1c6cbaca5ab7ca98420dfc018fa6533fbf31e9
JarateKing <scmacintosh@upei.ca>
2026-03-13 12:10:18 -0300

    more style work

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,6 +8,41 @@ end
 
 -- restyle
 config.font_size = 10
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.use_fancy_tab_bar = false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 config.color_scheme = "Ayu Mirage"
+config.colors = {
+	background = '#0B1118',
+	tab_bar = {
+		background = '#0B1118',
+		active_tab = {
+			bg_color = '#113354',
+			fg_color = 'silver'
+		},
+		inactive_tab = {
+			bg_color = '#0B1118',
+			fg_color = 'silver'
+		},
+		inactive_tab_hover = {
+			bg_color = '#0B1118',
+			fg_color = 'white'
+		
+		},
+		new_tab = {
+			bg_color = '#0B1118',
+			fg_color = 'silver'
+		},
+		new_tab_hover = {
+			bg_color = '#0B1118',
+			fg_color = 'white'
+		}
+	}
+}
 
 return config
