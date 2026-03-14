daae9971ce63e9a10efeb9cc8333e41e2958d78e
JarateKing <scmacintosh@upei.ca>
2026-03-14 16:23:28 -0300

    added directory hyperlink support with cse

--- .wezterm.lua
+++ .wezterm.lua
@@ -121,4 +121,44 @@ config.keys = {
 	}
 }
 
+-- directory/file hyperlink support
+-- modified from wezterm.org/recipes/hyperlinks.html#configuration
+local function is_shell(foreground_process_name)
+    local shell_names = { 'bash', 'bash.exe', 'zsh', 'zsh.exe', 'fish', 'fish.exe', 'sh', 'sh.exe', 'ksh', 'ksh.exe', 'dash', 'dash.exe' }
+    local process = string.match(foreground_process_name, '[^/\\]+$')
+        or foreground_process_name
+    for _, shell in ipairs(shell_names) do
+        if process == shell then
+            return true
+        end
+    end
+    return false
+end
+wezterm.on('open-uri', function(window, pane, uri)
+    local editor = 'nvim'
+    if uri:find '^file:' == 1 and not pane:is_alt_screen_active() then
+        -- We're processing an hyperlink and the uri format should be: file://[HOSTNAME]/PATH[#linenr]
+        -- Also the pane is not in an alternate screen (an editor, less, etc)
+        local url = wezterm.url.parse(uri)
+        if is_shell(pane:get_foreground_process_name()) then
+            pane:send_text(
+				wezterm.shell_join_args { 'cse', url.file_path } .. '\r'
+			)
+        else
+            -- No shell detected, we're probably connected with SSH, use fallback command
+            local edit_cmd = url.fragment
+                    and editor .. ' +' .. url.fragment .. ' "$_f"'
+                or editor .. ' "$_f"'
+            local cmd = '_f="'
+                .. url.file_path
+                .. '"; { test -d "$_f" && { cd "$_f" ; ls -a -p --hyperlink --group-directories-first; }; } '
+                .. '|| { test "$(file --brief --mime-type "$_f" | cut -d/ -f1 || true)" = "text" && '
+                .. edit_cmd
+                .. '; }; echo'
+            pane:send_text(cmd .. '\r')
+            return false
+        end
+    end
+end)
+
 return config
