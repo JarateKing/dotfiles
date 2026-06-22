29a122acf72fc7b0ae1ffebcd7eff81fbcbbf05e
JarateKing <scmacintosh@upei.ca>
2026-06-22 08:58:40 -0300

    support multiple regex and exclusionary regex

--- scripts/helpers/dict_search.py
+++ scripts/helpers/dict_search.py
@@ -2,14 +2,35 @@ import sys, re
 
 should_print = True
 should_count = False
+include_next = False
+exclude_next = False
+
+include_regex = []
+exclude_regex = []
 
 for arg in sys.argv[2:]:
-	if arg == '--only-count' or arg == '-c':
+	# priority:
+	# - the argument of `-i .` and `-e .`
+	# - standard arguments, including the first part of `-i` and `-e`
+	# - anything else is assumed to be a regex
+	if include_next:
+		include_next = False
+		include_regex.append(arg)
+	elif exclude_next:
+		exclude_next = False
+		exclude_regex.append(arg)
+	elif arg == '--only-count' or arg == '-c':
 		should_print = False
 		should_count = True
-	if arg == '--summary' or arg == '-s':
+	elif arg == '--summary' or arg == '-s':
 		should_print = True
 		should_count = True
+	elif arg == '--include' or arg == '-i':
+		include_next = True
+	elif arg == '--exclude' or arg == '-e':
+		exclude_next = True
+	else:
+		include_regex.append(arg)
 
 total = 0
 count = 0
@@ -20,7 +41,16 @@ with open(sys.argv[1]) as words:
 		
 		total += 1
 		
-		if (re.search(sys.argv[2], word)):
+		is_good = True
+		
+		for regex in include_regex:
+			if (not re.search(regex, word)):
+				is_good = False
+		for regex in exclude_regex:
+			if (re.search(regex, word)):
+				is_good = False
+		
+		if is_good:
 			count += 1
 			if should_print:
 				print(word)
